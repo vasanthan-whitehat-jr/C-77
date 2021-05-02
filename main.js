745296bd7009f0391f47dce5571177d8ba017bc3
vasanthan-whitehat-jr <82252864+vasanthan-whitehat-jr@users.noreply.github.com>
2021-05-02 22:04:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("My first console project");
+
+var first_name="T.vasanthan";
+var father_name="ML.thangaranjan";
+
+var full_name=first_name.concat(father_name);
+console.log(full_name);
+
+var a=10;
+var b=5;
+
+var add=a+b;
+console.log(add);
+
+var sub=a-b;
+console.log(sub);
+
+var multiply=a*b;
+console.log(multiply);
+
+var divide = a/b;
+console.log(divide);
+
+var 
